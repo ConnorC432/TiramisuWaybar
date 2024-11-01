68ad7adff0d38bf354abca31c6636ec285bb8135
ConnorC432 <connor.chetham@gmail.com>
2024-11-01 13:35:39 +0000

    changed notification clear function

--- app.py
+++ app.py
@@ -6,22 +6,25 @@ import time
 import queue
 
 
-wait_time = 1
+wait_time = 5
 
 
 def read_lines(process, process_queue):
     for line in process.stdout:
         notif_json = json.loads(line.strip())
-        notif = f"\uf0f3  {notif_json['summary'][:50]} | {notif_json['body'][:100]}"
+        notif = f"{notif_json['summary'][:50]} | {notif_json['body'][:100]}"
         process_queue.put(notif)
     process.stdout.close()
 
 
-def clear_notif():
-    print("\uf0f3", flush=True)
+def clear_notif(notif_text):
+    for i in range(len(notif_text) - 1, -1, -1):
+        print("\uf0f3  " + notif_text[0:i:1], flush=True)
+        time.sleep(0.01)
 
 
 def main():
+    global notif_text
     tiramisu_process = subprocess.Popen(
         "/usr/bin/tiramisu -j",
         shell=True,
@@ -40,13 +43,14 @@ def main():
 
     while True:
         if not process_queue.empty():
-            print(process_queue.get(), flush=True)
+            notif_text = process_queue.get()
+            print("\uf0f3  " + notif_text, flush=True)
             timed_out = False
             last_notif = time.time()
 
         elif time.time() - last_notif > wait_time and not timed_out:
             timed_out = True
-            clear_notif()
+            clear_notif(notif_text)
 
         else:
             time.sleep(0.1)
@@ -54,5 +58,5 @@ def main():
 
 if __name__ == "__main__":
     os.system("killall tiramisu")
-    print("\uf0f3", flush=True)
+    print("\uf0f3  ", flush=True)
     main()
